ded4f81f480c179ca8693cf7d0090726cd072c85
Chingsan53 <cly53@csu.fullerton.edu>
2023-01-28 12:57:37 -0800

    update index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ module.exports = {
     extend: {
       colors: {
         'slightly-slate': '#E1D7C6'
+      },
+      spacing: {
+        "big": "48rem"
       }
     },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    fontFamily: {
+      nunito: ['Nunito', 'sans-serif']
+    }
   },
   plugins: [require('flowbite/plugin')],
   darkMode: 'class'
